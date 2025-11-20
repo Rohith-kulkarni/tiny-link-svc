fa320a4c813607793acf260ed5fecec2b047bef3
Rohith Kumar <sairohitkulkarni839@gmail.com>
2025-11-20 16:57:07 +0530

    fix: dotenv remove from auth.js

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,7 +1,6 @@
 import { Router } from "express";
 import { signup, login, logout } from "../controllers/auth.controller.js";
 import { authRequired } from "../middleware/auth.middleware.js";
-dotenv.config();
 
 const router = Router();
 
@@ -10,7 +9,7 @@ router.post("/login", login);
 router.post("/logout", logout);
 
 // NEW: Restore user from cookie on page refresh
-router.get(`${process.env.BASE_URL}/me`, authRequired, (req, res) => {
+router.get("/me", authRequired, (req, res) => {
   res.json({
     ok: true,
     user: req.user, // comes from authRequired middleware
