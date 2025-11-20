6ddecaa8a2472bdee38b74ba2835ec0fc34283a5
Rohith Kumar <sairohitkulkarni839@gmail.com>
2025-11-20 12:40:17 +0530

    fix: environment variables change

--- src/server.js
+++ src/server.js
@@ -22,7 +22,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:3001",
+    origin: process.env.FRONTEND_URL,
     credentials: true,
   })
 );
